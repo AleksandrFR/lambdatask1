bff6c22d49aa1265c347a5e9d859a60eae7a99c9
AleksandrFR <79194849399@yandex.ru>
2022-02-21 21:32:57 +0300

    fix errors Main.java

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -3,10 +3,13 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
-        int a = calc.plus.apply(1, 2);
-        int b = calc.minus.apply(1, 1);
-        int c = calc.devide.apply(a, b); // тут будет арифметическая ошибка - деление на ноль,
-                                         // для корректной работы программы обнрнём в try.. catch
-        calc.println.accept(c);
+        try {
+            int a = calc.plus.apply(1, 2);
+            int b = calc.minus.apply(1, 1);
+            int c = calc.devide.apply(a, b);
+            calc.println.accept(c);
+        } catch (ArithmeticException exception) {
+            System.out.println("на ноль делить нельзя!!!");
+        }
     }
 }
