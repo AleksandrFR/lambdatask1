f72f263403c582c5953de9702e059c36c193a3a2
AleksandrFR <79194849399@yandex.ru>
2022-02-21 21:22:15 +0300

    add Main.java & Calculator.java

---
+++ src/ru/netology/Main.java
@@ -0,0 +1,15 @@
+package ru.netology;
+
+public class Main {
+    public static void main(String[] args) {
+        Calculator calc = Calculator.instance.get();
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.devide.apply(a, b); // тут будет арифметическая ошибка - деление на ноль,
+                                         // для корректной работы программы обнрнём в try.. catch
+        calc.println.accept(c);
+        {
+            System.out.println("на ноль делить нельзя!!!");
+        }
+    }
+}
