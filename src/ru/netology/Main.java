b1552f6e72bc633edbb8b987709761fad8703928
AleksandrFR <79194849399@yandex.ru>
2022-02-22 21:43:27 +0300

    errors prevention Main.java

--- src/ru/netology/Main.java
+++ src/ru/netology/Main.java
@@ -3,13 +3,9 @@ package ru.netology;
 public class Main {
     public static void main(String[] args) {
         Calculator calc = Calculator.instance.get();
-        try {
             int a = calc.plus.apply(1, 2);
             int b = calc.minus.apply(1, 1);
-            int c = calc.devide.apply(a, b);
+            int c = calc.devide.apply(a, b);// тут будет деление на ноль при делении на ноль результат бесконечность
             calc.println.accept(c);
-        } catch (ArithmeticException exception) {
-            System.out.println("на ноль делить нельзя!!!");
-        }
     }
 }
